258a2949ab0f3f54ac80f1c5c9a3339e8bfaf08e
Shahar Zidon <shaharsmacbookpro@Shahars-MacBook-Pro.local>
2022-11-16 10:21:24 +0200

    fixed port issue for server

--- server.js
+++ server.js
@@ -1,18 +1,70 @@
-const express = require('express');
-const http = require('http');
-const app = express();
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
+
+
+const app = express();
 const server = http.createServer(app);
+const io = socketio(server)
 
-
-
-// app.get('/', (req, res) => {
-//     res.send('chello world');
-// })
-
+//Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000 || process.env.PORT;
+const botName = 'Chatcord Bot';
+
+//Runs when client connects
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room)
+
+        //welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+        // Broadcast when a user connects
+        socket.broadcast.to(user.room)
+            .emit(
+                'message',
+                formatMessage(botName, `${user.username} has joined to the chat`)
+            );
+        //Send user and a room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    })
+
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+    });
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has l
+            eft the chat`));
+
+            //Send user and a room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
+    });
+});
+
+
+
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Youre listening to port ${PORT}`);
-})
+})   
